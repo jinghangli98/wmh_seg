7ea220859723c5d499c983c4c364774e905dba19
jinghangli98 <jil202@pitt.edu>
2023-05-25 13:53:17 -0400

    3d segmentation done

--- wmh_pytorch.py
+++ wmh_pytorch.py
@@ -9,6 +9,7 @@ from einops import rearrange
 import sys
 import pdb
 import torchio as tio
+import matplotlib.pyplot as plt
 
 in_path=sys.argv[1]
 out_path=sys.argv[2]
@@ -16,6 +17,14 @@ gpu=sys.argv[3]
 wmh_seg_home=sys.argv[4]
 verbose=sys.argv[5]
 pmb=sys.argv[6]
+
+def reduceSize(prediction):
+    arg = prediction > 0.1
+    out = np.zeros(prediction.shape)
+    out[arg] = 1
+    
+    return out
+    
 def wmh_seg(in_path, out_path, train_transforms, device, mode):
 
 
@@ -31,12 +40,11 @@ def wmh_seg(in_path, out_path, train_transforms, device, mode):
         
     else:
         img_orig = nib.load(in_path)
-        transform = tio.transforms.Resize((255,256,257))
-        img = transform(img_orig)
+        img = train_transforms(img_orig)
         input = np.squeeze(img.get_fdata())
-        prediction_axial = np.zeros((255,256,257))
-        prediction_cor = np.zeros((255,256,257))
-        prediction_sag = np.zeros((255,256,257))
+        prediction_axial = np.zeros((256,256,256))
+        prediction_cor = np.zeros((256,256,256))
+        prediction_sag = np.zeros((256,256,256))
         input = torch.tensor(input)
         affine = img.affine
         input = torch.unsqueeze(input, 1)
@@ -47,12 +55,12 @@ def wmh_seg(in_path, out_path, train_transforms, device, mode):
     
     if verbose == "True":
         for idx in tqdm(range(input.shape[0])):
-            axial_img = prediction_input[idx].repeat(3,1,1)
+            axial_img = rearrange(prediction_input[:,:,:,idx], 'd0 d1 d2 -> d1 d0 d2').repeat(3,1,1)
             cor_img = rearrange(prediction_input[:,:,idx,:], 'd0 d1 d2 -> d1 d0 d2').repeat(3,1,1)
-            sag_img = rearrange(prediction_input[:,:,:,idx], 'd0 d1 d2 -> d1 d0 d2').repeat(3,1,1)
+            sag_img = prediction_input[idx,:,:,:].repeat(3,1,1)
             prediction_axial[:, :, idx] = model(torch.unsqueeze(axial_img, 0).float()).squeeze().detach().cpu().numpy()
-            prediction_cor[:, :, idx] = model(torch.unsqueeze(cor_img, 0).float()).squeeze().detach().cpu().numpy()
-            prediction_sag[:, :, idx] = model(torch.unsqueeze(sag_img, 0).float()).squeeze().detach().cpu().numpy()
+            prediction_cor[:, idx, :] = model(torch.unsqueeze(cor_img, 0).float()).squeeze().detach().cpu().numpy()
+            prediction_sag[idx, :, :] = model(torch.unsqueeze(sag_img, 0).float()).squeeze().detach().cpu().numpy()
         prediction = prediction_axial + prediction_cor + prediction_sag
     elif verbose != "True":
         for idx in range(input.shape[0]):
@@ -60,9 +68,7 @@ def wmh_seg(in_path, out_path, train_transforms, device, mode):
             prediction[:, :, idx] = model(torch.unsqueeze(input_image, 0).float()).squeeze().detach().cpu().numpy()
 
     #saving images
-    arg = prediction > 0.5
-    out = np.zeros(prediction.shape)
-    out[arg] = 1
+    out = reduceSize(prediction)
     if gpu == "True":
         img_fit = input.squeeze().cpu().numpy()
     else:
@@ -74,18 +80,14 @@ def wmh_seg(in_path, out_path, train_transforms, device, mode):
         transform = tio.transforms.Resize((img_orig.get_fdata().shape[0], img_orig.get_fdata().shape[1], img_orig.get_fdata().shape[2]))
         out = transform(np.expand_dims(out,0))
         out = np.squeeze(out)
-        # 
         
     else:
-        train_transforms = torchvision.transforms.Compose([
-                    torchvision.transforms.ToTensor(),
-                    torchvision.transforms.Resize((img.get_fdata().shape[0], img.get_fdata().shape[1],)),
-                    torchvision.transforms.CenterCrop((img.get_fdata().shape[0], img.get_fdata().shape[1],)),])
-        out = train_transforms(out).detach().cpu().numpy()
-        out = rearrange(out, 'd0 d1 d2 -> d1 d2 d0')
+        pdb.set_trace()
+        transform = tio.transforms.Resize((img_orig.get_fdata().shape[0], img_orig.get_fdata().shape[1], img_orig.get_fdata().shape[2]))
+        out = transform(np.expand_dims(out, 0))
+        out = np.squeeze(out)
     
-    nii_seg = nib.Nifti1Image(out, affine=affine)
-
+    nii_seg = nib.Nifti1Image(out, affine=img_orig.affine)
     nib.save(nii_seg, out_path)
 
 filename=in_path.split('/')[-1]
@@ -101,10 +103,7 @@ else:
     print('Configuring model on CPU')
 
 if pmb == "False":
-    train_transforms = torchvision.transforms.Compose([ 
-                        torchvision.transforms.ToTensor(),
-                        torchvision.transforms.Resize((255, 256,)), 
-                        ])
+    train_transforms = transform = tio.transforms.Resize((256,256,256))
     
     model = torch.load(f"{wmh_seg_home}/ChallengeMatched_Unet_mit_b5.pth", map_location=device)
     model.eval()
